2a186de94063d3e8696d3489938f961a8efeb10c
Pattles <57507687+Pattles@users.noreply.github.com>
2024-01-16 15:19:17 -0500

    added marketplace

--- IOS_App.swift
+++ IOS_App.swift
@@ -7,7 +7,7 @@
 - Remove player +
 */ 
 
-let reservedKeywords: [String] = ["defaultplayer"]
+let reservedKeywords: [String] = ["defaultplayer", "defaultitem"]
 
 func formatName(toFormat name: String) -> String {
   let formattedName = name.prefix(1).uppercased() + name.lowercased().dropFirst()
@@ -32,8 +32,8 @@ struct Player {
 var players: [Player] = []
 
 func findPlayer(byName name: String) -> Player {
-    if let player = players.first(where: { $0.name == name }) {
-        return player
+    if let foundPlayer = players.first(where: { $0.name == name }) {
+        return foundPlayer
     } else {
         // If the player isn't found:
         return Player(name: "DefaultPlayer", wins: 0)
@@ -47,7 +47,7 @@ func addPlayer(playerName name: String, currentWins wins: Int) {
   }
   
   // Checking if the player already exists, if not, adding a new player
-  let existingPlayer = findPlayer(byName: name)
+  let existingPlayer: Player = findPlayer(byName: name)
   
   // "DefaultPlayer" means the player doesn't exist
   if existingPlayer.name == "DefaultPlayer" {
@@ -62,7 +62,7 @@ func addPlayer(playerName name: String, currentWins wins: Int) {
 
 func deletePlayer(playerName name: String) {
   // Checking if player already exists, if so, removes player
-  let existingPlayer = findPlayer(byName: name)
+  let existingPlayer: Player = findPlayer(byName: name)
 
   if existingPlayer.name != "DefaultPlayer" {
     guard let indexToRemove = players.firstIndex(where: { $0.name == name }) else {
@@ -79,17 +79,17 @@ func deletePlayer(playerName name: String) {
 
 // --------- MARKETPLACE ---------
 /*
-- Add item to shop
-- Remove item from shop
-- Update item price
+- Add item to shop +
+- Remove item from shop +
+- Update item price +
 - Buy item
-- View item (itemName, price, seller)
+- View all items by seller +
+
 
 (Optional)
 - Add item description
 - Remove item description
 - Update item description
-- View item (+description)
 */
 
 
@@ -105,27 +105,83 @@ struct Item {
   }
 }
 
-// TODO
-/*
-func addItem(itemName itemName: String, winsPrice price: Int, seller seller: Player) {
+var allItems: [Item] = []
 
+func findItem(byName name: String, seller: Player) -> Item {
+  if let foundItem = allItems.first(where: { $0.itemName == name }) {
+        return foundItem
+    } else {
+        // If the item isn't found:
+        return Item(itemName: "DefaultItem", winsPrice: 0, seller: Player(name: "DefaultPlayer", wins: 0))
+    }
 }
-*/
-print("Hello world!")
 
-// --------- TESTING MARKETPLACE ---------
+func addItem(itemName: String, price: Int, seller: Player) {
+  // In case the player decides to use a reserved keyword
+  if reservedKeywords.contains(itemName.lowercased()) {
+    return print("This keyword is reserved.")
+  }
 
-// var allItems: [Item] = [Item(itemName: "glasses", winsPrice: 10, seller: Player(name: "Koan", wins: 10))]
+  let existingItemBySeller: Item = findItem(byName: itemName, seller: seller)
 
-// print(allItems)
+  // "DefaultItem" means the item doesn't exist
+  if existingItemBySeller.itemName == "DefaultItem" {
+    allItems.append(Item(itemName: itemName, winsPrice: price, seller: seller))
+    return print("Successfully added \(itemName) to the shop.")
+  }
+
+  return print("\(itemName) already exists in the shop.")
+}
+
+
+func removeItem(itemName: String, seller: Player) {
+  // In case the player decides to use a reserved keyword
+  if reservedKeywords.contains(itemName.lowercased()) {
+    return print("This keyword is reserved.")
+  }
+
+  let existingItemBySeller: Item = findItem(byName: itemName, seller: seller)
+
+  if existingItemBySeller.itemName != "DefaultItem" {
+    guard let indexToRemove = allItems.firstIndex(where: { $0.itemName == itemName && $0.seller.name == seller.name }) else {
+      // If the item doesn't exist, does nothing
+      return print("\(itemName) doesn't exist in the shop.")
+    }
+
+    allItems.remove(at: indexToRemove)
+    return print("Successfully removed \(itemName) from the shop.")
+  }
+}
+
+func getAllItemsOfSeller(seller: Player) -> [Item] {
+  var allItemsOfSeller: [Item] = []
+
+  // Getting all the items of seller
+  for item in allItems {
+    if item.seller.name == seller.name {
+      allItemsOfSeller.append(item)
+    }
+  }
+  return allItemsOfSeller
+}
+
+// Could be redundant, I'm not sure
+func displayItems(allItemsOfSeller: [Item]) {
+  print("\(allItemsOfSeller[0].seller.name) is selling the following items:")
+  for item in allItemsOfSeller {
+    print("\(item.itemName): \(item.winsPrice) wins")
+  }
+  return
+}
 
 // --------- TESTING FUNCTIONS ---------
 
 // Adding a player
-// addPlayer(playerName: formatName(toFormat: "Patrik"), currentWins: 10)
-// addPlayer(playerName: formatName(toFormat: "Koan"), currentWins: 10)
+addPlayer(playerName: formatName(toFormat: "Koan"), currentWins: 10)
+addPlayer(playerName: formatName(toFormat: "Patrik"), currentWins: 10)
 
-// var koan: Player = findPlayer(byName: formatName(toFormat: "Koan"))
+var koan: Player = findPlayer(byName: formatName(toFormat: "Koan"))
+var patrik: Player = findPlayer(byName: formatName(toFormat: "Patrik"))
 
 // Adding & removing wins to a player
 // koan.addWins(numOfAddedWins: 3)
@@ -139,13 +195,16 @@ print("Hello world!")
 // deletePlayer(playerName: formatName(toFormat: "Koan"))
 // deletePlayer(playerName: formatName(toFormat: "DefaultPlayer"))
 
-// print(players)
+// --------- TESTING MARKETPLACE ---------
 
+addItem(itemName: formatName(toFormat: "glasses"), price: 3, seller: koan)
+addItem(itemName: formatName(toFormat: "phone"), price: 7, seller: koan)
 
+removeItem(itemName: formatName(toFormat: "phone"), seller: koan)
 
+var allItemsOfKoan: [Item] = getAllItemsOfSeller(seller: koan)
+displayItems(allItemsOfSeller: allItemsOfKoan)
 
-
-
-
+var koanGlasses = findItem(byName: formatName(toFormat: "glasses"), seller: koan)
 
 
